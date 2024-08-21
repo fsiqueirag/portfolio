d3e7cd898885dc99f614b7c62b76cf5addce001e
fsiqueirag <fabrizio.siqueira.g@gmail.com>
2024-08-21 20:57:38 -0300

    commit

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
 export { default as Github } from "./icons/Github.astro";
 export { default as Linkedin } from "./icons/Linkedin.astro";
 export { default as Email } from "./icons/Email.astro";
+export { default as Home } from "./icons/Home.astro";
