521f9f40db625d05a55097f8f4217fa22c4badec
fsiqueirag <fabrizio.siqueira.g@gmail.com>
2024-08-21 21:06:28 -0300

    Restore deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
-  server: {
-    host: "0.0.0.0",
-  },
   integrations: [tailwind()],
 });
