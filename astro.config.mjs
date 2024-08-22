59aa02e845d43b56dd23394d771e3918c862703b
fsiqueirag <fabrizio.siqueira.g@gmail.com>
2024-08-21 21:01:37 -0300

    Deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,12 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  server: {
+    host: "0.0.0.0",
+  },
   integrations: [tailwind()],
 });
