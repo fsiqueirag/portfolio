230bcf9d9fcf8f2eb255e1e5581f83f8d2f35fc7
fsiqueirag <fabrizio.siqueira.g@gmail.com>
2024-08-22 16:03:34 -0300

    Add fonts and animations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,11 +12,26 @@ export default {
       },
       animation: {
         "height-grow": "heightGrow 3s linear forwards",
+        "fade-in": "fadeIn 1s ease-in-out both",
+        "fade-right": "fadeRight 1s ease-in-out both",
+        "fade-left": "fadeLeft 1s ease-in-out both",
       },
       keyframes: {
         heightGrow: {
           "0%": { height: 0 },
           "100%": { height: "100vh" },
+        },
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        fadeLeft: {
+          "0%": { opacity: 0, transform: "translateX(40px)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
+        },
+        fadeRight: {
+          "0%": { opacity: 0, transform: "translateX(-40px)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
         },
       },
     },
