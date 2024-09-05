b5dbc791bf16a499f8cb8b92fe20e5ab9b3848c6
fsiqueirag <fabrizio.siqueira.g@gmail.com>
2024-09-04 22:27:56 -0300

    Fine tuning

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({ applyBaseStyles: false }), react(), sitemap()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en"],
+  },
 });
